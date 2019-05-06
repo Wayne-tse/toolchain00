2edb33252f3e317b85301f54464b5cd75c52e91b
Wayne Tse <wt41@students.waikato.ac.nz>
2019-05-06 03:15:19 +0000

    Changed units

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -7,7 +7,7 @@ var request = REQUEST.defaults( {
     strictSSL: false
 });
 
-var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
+var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric";//units are now metric
 
 exports.getWeather = function(req, res) {
 	var zip = req.query.zip;
@@ -15,8 +15,9 @@ exports.getWeather = function(req, res) {
 		return res.status(400).send('zip missing');
 	}
 
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
 
+	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	//var aurl = OPENWEATHERURL + '&q=' + city + 'nz';
 	request({
 		method: 'GET',
         url: aurl,
