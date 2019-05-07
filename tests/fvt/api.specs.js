9161fae63a8ab1c6c011533c27486ca3bb02972d
Wayne Tse <wt41@students.waikato.ac.nz>
2019-05-07 01:11:11 +0000

    test changes

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -15,7 +15,7 @@
 
     describe('Get Weather', function() {
     	
-    	it('with valid city', function(done) {
+    	it('with valid zip', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
             return done();
@@ -37,13 +37,14 @@
           	} else {
               assert.equal(resp.statusCode, 200);
               var pbody = JSON.parse(body);
-              assert((pbody.name === 'Wellington') || (pbody.country === 'NZ'), "names does not match request");
+              //assert((pbody.name === 'Wellington') || (pbody.country === 'NZ'), "names does not match request");
+              assert((pbody.city === 'Anderson Mill') || (pbody.city === 'Round Rock'), "City name does not match");
               done();
             }
         });
     	});
 
-      it('without city code', function(done) {
+      it('without zip code', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
             return done();
@@ -61,21 +62,23 @@
         });
     	});
 
-      it('with another valid city', function(done) {
+      it('with another valid zip', function(done) {
         if(!appUrl) {
             assert.fail("Environment variable APP_URL is not defined");
             return done();
         }
         request({
       		method: 'GET',
-              url: appUrl + '&q=Hamilton,nz'
+              //url: appUrl + '&q=Hamilton,nz'
+          		 url: appUrl + '/api/v1/getWeather?zip=78641'
           }, function(err, resp, body) {
           	if(err) {
           		assert.fail('Failed to get the response');
           	} else {
               assert.equal(resp.statusCode, 200);
               var pbody = JSON.parse(body);
-              assert(pbody.city === 'Hamilton', "City name does not match");
+              //assert(pbody.city === 'Hamilton', "City name does not match");
+               assert(pbody.city === 'Round Rock', "City name does not match");
               done();
             }
         });
