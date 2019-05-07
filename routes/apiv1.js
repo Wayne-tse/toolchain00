f01c7dfd89658c919d9bb4f2fb61497e96100525
Wayne Tse <wt41@students.waikato.ac.nz>
2019-05-07 06:04:55 +0000

    added API functionality

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -34,7 +34,7 @@ exports.getWeather = function(req, res) {
     		//console.error("Failed to send request to openweathermap.org", err);
     	} else {
     		if(body.cod === 200) {
-    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' F';
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
     			var response = {city: body.name, weather: weath};
     			return res.status(200).send(response);
     		} else {
@@ -76,6 +76,35 @@ exports.getWeather2 = function(req, res) {
 
 };
 router.get('/getWeather2', exports.getWeather2);
+
+exports.getWeatherCity = function(req, res) {
+	var city = req.query.zip;
+	if( (city === null) || (typeof(city) === 'undefined') ) {
+		return res.status(400).send('city missing');
+	}
+
+	var aurl = OPENWEATHERURL + '&q=' + city + ',nz';
+	request({
+		method: 'GET',
+        url: aurl,
+  		json: true
+    }, function(err, resp, body) {
+    	if(err) {
+    		res.status(400).send('Failed to get the data');
+    		//console.error("Failed to send request to openweathermap.org", err);
+    	} else {
+    		if(body.cod === 200) {
+    			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
+    			var response = {city: body.name, weather: weath};
+    			return res.status(200).send(response);
+    		} else {
+                return res.status(400).send({msg:'Failed'});
+            }
+    	}
+    });
+
+};
+router.get('/getWeatherCity', exports.getWeatherCity);
 
 
 exports.router = router;
