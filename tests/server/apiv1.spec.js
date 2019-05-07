44afbf1b077ff7aae5ad7b3202cfc745fff31158
Wayne Tse <wt41@students.waikato.ac.nz>
2019-05-07 07:07:03 +0000

    unit tests for city weather

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -1,14 +1,13 @@
 
+/*eslint-disable no-extra-parens */
 (function () {
 
   'use strict';
 
-	var requireHelper = require('./requireHelper');
+var requireHelper = require('./requireHelper');
   var apiv1 = requireHelper.require('tests/coverage/instrumented/routes/apiv1');
   var assert = require('chai').assert;
   var sinon = require('sinon');
-
-
 
   // create mock request and response
   var reqMock = {};
@@ -193,6 +192,95 @@
       apiv1.__set__("request", request);
 
       apiv1.getWeather2(reqMock, resMock);
+
+      assert(resMock.status.lastCall.calledWith(200), 'Unexpected response:' + resMock.status.lastCall.args);
+      assert(resMock.send.lastCall.args[0].city === 'El Paso', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
+      assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 78 F', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
+    });
+  });
+  
+    describe('Get Weather city', function() {
+
+    it('with without city code', function() {
+      reqMock = {
+        query: {
+
+        }
+      };
+
+      apiv1.getWeatherCity(reqMock, resMock);
+
+      assert(resMock.status.lastCall.calledWith(400), 'Unexpected status code:' + resMock.status.lastCall.args);
+    });
+
+    it('with valid zip code and error from request call', function() {
+      reqMock = {
+        query: {
+          //zip: 79968
+			city: 'Hamilton'        
+        }
+      };
+
+      var request = function( obj, callback ){
+        callback("error", null, null);
+      };
+
+      apiv1.__set__("request", request);
+
+      apiv1.getWeatherCity(reqMock, resMock);
+
+      assert(resMock.status.lastCall.calledWith(400), 'Unexpected response:' + resMock.status.lastCall.args);
+      assert(resMock.send.lastCall.calledWith('Failed to get the data'), 'Unexpected response:' + resMock.send.lastCall.args);
+    });
+
+    it('with incomplete zip code', function() {
+      reqMock = {
+        query: {
+          //zip: 79968
+        	city: 'Hamilton'
+        }
+      };
+
+      var request = function( obj, callback ){
+        callback(null, null, {});
+      };
+
+      apiv1.__set__("request", request);
+
+      apiv1.getWeatherCity(reqMock, resMock);
+
+      assert(resMock.status.lastCall.calledWith(400), 'Unexpected response:' + resMock.status.lastCall.args);
+      assert(resMock.send.lastCall.args[0].msg === 'Failed', 'Unexpected response:' + resMock.send.lastCall.args);
+    });
+
+    it('with valid zip code', function() {
+      reqMock = {
+        query: {
+          city: 'Hamilton'
+          //zip: 79968
+        }
+      };
+
+      var body = {
+        cod: 200,
+        name: 'El Paso',
+        weather: [
+          {
+            main: 'cold'
+          }
+        ],
+        main: {
+          temp: 78
+        }
+      };
+
+      var request = function( obj, callback ){
+        callback(null, null, body);
+      };
+
+      apiv1.__set__("request", request);
+
+      apiv1.getWeatherCity(reqMock, resMock);
 
       assert(resMock.status.lastCall.calledWith(200), 'Unexpected response:' + resMock.status.lastCall.args);
       assert(resMock.send.lastCall.args[0].city === 'El Paso', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
